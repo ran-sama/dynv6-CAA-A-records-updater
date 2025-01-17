857d6456e820583724ed3a59866d386d8f3e216a
Yakumo Ran <31311154+ran-sama@users.noreply.github.com>
2025-01-17 11:47:12 +0100

    Create get_records.py

---
+++ get_records.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import requests
+
+zone_ID = '4000000'
+API_token = 'supersecrettoken'
+API_URI = 'https://dynv6.com/api/v2/zones/' + zone_ID + '/records'
+API_header = {'Authorization': 'Bearer ' + API_token, 'content-type': 'application/json'}
+
+my_post =  requests.get(url=API_URI,headers=API_header)
+my_reply = my_post.content.decode('utf-8')
+print(my_reply)
